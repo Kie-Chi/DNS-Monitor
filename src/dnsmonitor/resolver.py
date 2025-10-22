7fd213cc011866f72c636f56506860075edad3ec
Kie-Chi <137579437@qq.com>
2025-10-22 20:56:20 +0800

    fix(resolver): fix attr analyze_path? is analyzed_path.

--- src/dnsmonitor/resolver.py
+++ src/dnsmonitor/resolver.py
@@ -579,7 +579,7 @@ class ResolverMonitor:
         else:
             transaction.timeout()
 
-        transaction.analyze_path = transaction.analyze_path(transaction)
+        transaction.analyzed_path = transaction.analyze_path(transaction)
         
         try:
             self.output_queue.put_nowait(transaction)
